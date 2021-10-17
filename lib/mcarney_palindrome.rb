5cc594bb68a27271c37519e2da6ebc61df69e086
Mannix <mannixcom@gmail.com>
2021-10-17 13:40:12 +0100

    Finish working and refactoring palindrome method

--- lib/mcarney_palindrome.rb
+++ lib/mcarney_palindrome.rb
@@ -2,7 +2,17 @@
 
 require_relative "mcarney_palindrome/version"
 
-module McarneyPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
