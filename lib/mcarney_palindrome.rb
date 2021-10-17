e5ee56157f359cad09359a4442eeada480942bb5
Mannix <mannixcom@gmail.com>
2021-10-17 12:30:47 +0100

    first update

---
+++ lib/mcarney_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mcarney_palindrome/version"
+
+module McarneyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mcarney_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module McarneyPalindrome
+  VERSION = "0.1.0"
+end
